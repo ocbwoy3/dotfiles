f762de0d2330369f064ce821977d9e2d6efcc9ee
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-07-27 21:57:57 +0300

    latvian keyboard now works, and better music thingy

--- scripts/waybar-music-status.sh
+++ scripts/waybar-music-status.sh
@@ -3,7 +3,9 @@
 SONG="$(playerctl metadata title -s)"
 
 if [ ${#SONG} -gt 1 ] ; then
-    printf "$(playerctl metadata artist) — $SONG" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g"
+	BRUH1=$( echo "  $(playerctl metadata artist) — $SONG" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g" )
+	BRUH2=$( echo "$(playerctl metadata album)" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g" )
+	printf "{\"text\": \"$BRUH1\", \"tooltip\": \"$BRUH2\" }"
 else
-    printf ""
+	printf "{\"text\": \"lol\", \"tooltip\": \" \" }"
 fi
