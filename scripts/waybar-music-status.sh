298b6b892953602788a2260a65d16ff9f4a9a4c4
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-08-01 12:49:40 +0300

    wtf

--- scripts/waybar-music-status.sh
+++ scripts/waybar-music-status.sh
@@ -7,5 +7,5 @@ if [ ${#SONG} -gt 1 ] ; then
 	BRUH2=$( echo "$(playerctl metadata album)" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g" )
 	printf "{\"text\": \"$BRUH1\", \"tooltip\": \"$BRUH2\" }"
 else
-	printf "{\"text\": \"lol\", \"tooltip\": \" \" }"
+	printf "{\"text\": \" \", \"tooltip\": \" \" }"
 fi
