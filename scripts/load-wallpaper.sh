7b24e5cf19febca245ea00a1a2ac98590058d365
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-08-08 17:14:24 +0300

    window title for roblox

--- scripts/load-wallpaper.sh
+++ scripts/load-wallpaper.sh
@@ -3,6 +3,8 @@
 WALLPAPER=$(cat $HOME/.cache/.wallpaper)
 
 rm -r ~/.cache/wal
+
 wal -stn -a 90 -i $HOME/wallpaper/$WALLPAPER
+
 swww img $HOME/wallpaper/$WALLPAPER --transition-step 120 --transition-fps 120 --transition-type grow --transition-pos top-right --transition-duration 1.5 &
 bash ~/dotfiles/scripts/hot-reload.sh
--- scripts/select-wallpaper.sh
+++ scripts/select-wallpaper.sh
@@ -2,9 +2,17 @@
 
 WALLPAPER=$(ls $HOME/wallpaper/ | wofi --show dmenu)
 
+if [ ${#WALLPAPER} -lt 1] ; then
+	exit
+fi
+
+rm $HOME/.cache/.wallpaper
+
 echo "$WALLPAPER" >> $HOME/.cache/.wallpaper
 
-rm -r ~/.cache/wal
+rm -r $HOME/.cache/wal
+
 wal -stn -a 90 -i $HOME/wallpaper/$WALLPAPER
+
 swww img $HOME/wallpaper/$WALLPAPER --transition-step 120 --transition-fps 120 --transition-type grow --transition-pos top-right --transition-duration 1.5 &
 bash ~/dotfiles/scripts/hot-reload.sh
