ca8bf9fab8fd37e28eedeac64aa95bd95462e2f4
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-09-07 16:00:42 +0300

    a

--- scripts/waybar-music-status.sh
+++ scripts/waybar-music-status.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+function _processTitle {
+	sed -r ""
+	# wip this shit	
+	# sed -r "s/(?!(.*(Eurobeat).*))(\(feat\. .*\)$| \- Friday Night Funkin'?:?.*$|(.*(Remix|Edited).*)$| \(.*Funkin.*\)$)/"
+}
+
 SONG="$(playerctl metadata title -s)"
 
 if [ ${#SONG} -gt 0 ] ; then
-	BRUH1=$( echo "  $(playerctl metadata artist | sed -r "s/( \& Minecraft$)//") — $(playerctl metadata title -s)" | sed -r "s/(\(.*Friday Night.*)|( (\-) Friday Night Funkin'?:? (.*)|\(feat\. (.*)\)| ?\((.*)(Funkin|Edited)(.*)\))$//g" | sed -r "s/\(?(!(Eurobeat))([a-zA-Z0-9]*? ?(R|r)emix|(E|e)dited ?[a-zA-Z0-9]*?|[a-zA-Z0-9]* ?(M|m)ix)\)?//" | sed -r "s/(^[ ]*)|([ ]*$)//g" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g" )
+	BRUH1=$( echo "  $(playerctl metadata artist | sed -r "s/( \& Minecraft$)//") — $(playerctl metadata title -s)" | _processTitle | sed -r "s/(^[ ]*)|([ ]*$)//g" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g" )
 	BRUH2=$( echo "$(playerctl metadata album)" | sed -r 's/\&/\&amp;/g' | sed -r 's/\"/\&quot;/g' | sed -r "s/'/\&apos;/g" )
 	printf "{\"text\": \"$BRUH1\", \"tooltip\": \"$BRUH2\" }"
 else
